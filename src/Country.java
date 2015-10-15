32e9a2fe6ef69fe2cf42397938081f7c6b051e04
JackNel <jcneligan@gmail.com>
2015-10-15 12:58:18 -0400

    Create class Country

---
+++ src/Country.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Jack on 10/15/15.
+ */
+public class Country {
+    String name;
+    String abbr;
+
+    public Country(String name, String abbr) {
+        this.name = name;
+        this.abbr = abbr;CCC
+    }
+}
